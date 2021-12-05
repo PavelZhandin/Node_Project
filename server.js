064ea4affe143e95b689afc145219bfd0896d045
Pavel_Zhandin <pavelzhandin@gmail.com>
2021-12-05 12:20:57 +0300

    created complete CRUD app for posts , started refactoring

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Post = require('./models/post');
+const Contact = require('./models/Contact');
+const methodOverride = require('method-override');
+
+const postRoutes = require('./routes/post-routes');
+
+app.set('view engine', 'ejs');
 
 const app = express();
 const PORT = 3000;
 
-app.set('view engine', 'ejs');
 
 const db = 'mongodb+srv://Pavel:529440@cluster0.x0pym.mongodb.net/node-block?retryWrites=true&w=majority';
 
@@ -23,9 +28,12 @@ app.listen(PORT,  (error)=>{
 });
 
 app.use(express.static('styles'));
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
 app.use(express.urlencoded({extended: false}));
 
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res)=>{
   const title = "Home";
@@ -34,62 +42,18 @@ app.get('/', (req, res)=>{
 
 app.get('/contacts', (req, res)=>{
   const title = "Contacts";
-  const contacts = [
-    { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
-    { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
-    { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
-  ];
-  res.render(createPath('contacts'), {contacts, title});
+  Contact
+      .find()
+      .then((contacts)=> res.render(createPath('contacts'), {contacts, title}))
+      .catch((error) => {
+        console.log(error);
+        res.render(createPath('error'), {title: "Error"})
+      });
+ 
 })
 
 app.get('/about-us', (req, res)=>{
   res.redirect('/contacts');
-});
-
-
-app.get('/posts/:id', (req,res)=>{
-  const title = "Post";
-  const post = { 
-    id: "1",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.",
-    title: "Embedded post title",
-    date: '05.05.2021',
-    author: 'Yauhen',
-  };
-  res.render(createPath('post'), {title, post});
-});
-
-app.get('/posts', (req,res)=>{
-  const title = "Posts";
-  const posts = [
-    { 
-      id: '1', 
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quidem provident, dolores, vero laboriosam nemo mollitia impedit unde fugit sint eveniet, minima odio ipsum sed recusandae aut iste aspernatur dolorem.',
-      title: 'Post title',
-      date: '05.05.2021',
-      author: 'Yauhen',
-    }
-  ]
-  res.render(createPath('posts'), {title, posts});
-});
-
-
-app.post('/add-post', (req,res)=>{
-  const {title, author, text} = req.body;
-  const post = new Post({title , author , text});
-  post
-    .save()
-    .then((result) => res.send(result))
-    .catch((error) => {
-      console.log(error);
-      res.render(createPath('error'), {title: "Error"})
-    });
-  // res.render(createPath('post1'), {post, title});
-});
-
-app.get('/add-post', (req,res)=>{
-  const title = "Add post";
-  res.render(createPath('add-post'), {title});
 });
 
 
