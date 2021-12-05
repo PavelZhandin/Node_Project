b2990960c42a9e461686be0e15de28dc3000b52d
Pavel_Zhandin <pavelzhandin@gmail.com>
2021-12-05 14:05:55 +0300

    minor improvements

--- server.js
+++ server.js
@@ -1,59 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const methodOverride = require('method-override');
+const postRoutes = require('./routes/post-routes');
+const postApiRoutes = require('./routes/api-post-routes');
+const contactRoutes = require('./routes/contact-routes');
 const createPath = require('./helpers/create-path');
 
-
-const postRoutes = require('./routes/post-routes');
-const contactRoutes = require('./routes/contact-routes');
-const postApiRoutes = require('./routes/api-post-routes');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
-
-
-const db = 'mongodb+srv://Pavel:529440@cluster0.x0pym.mongodb.net/node-block?retryWrites=true&w=majority';
-
 mongoose
-  .connect(db)
-  .then((res)=> console.log('Successfuly connected to DB'))
-  .catch((error)=> console.log('Error connecting to'))
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
 
-
-
-app.listen(PORT,  (error)=>{
-  error ? console.error(error) : console.log(`listening to port ${PORT}`);
+app.listen(process.env.PORT, (error) => {
+  error ? console.log(error) : console.log(`listening port ${process.env.PORT}`);
 });
 
-app.use(express.static('styles'));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.static('styles'));
 
 app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  const title = 'Home';
+  res.render(createPath('index'), { title });
+});
 
 app.use(postRoutes);
 app.use(contactRoutes);
 app.use(postApiRoutes);
-app.get('/', (req, res)=>{
-  const title = "Home";
-  res.render(createPath('index'), {title});
-});
 
-
-app.get('/about-us', (req, res)=>{
-  res.redirect('/contacts');
-});
-
-
-app.use((req, res)=>{
-  const title = 'Error';
+app.use((req, res) => {
+  const title = 'Error Page';
   res
-  .status(404)
-  .render(createPath('error'), {title});
-})
+    .status(404)
+    .render(createPath('error'), { title });
+});
